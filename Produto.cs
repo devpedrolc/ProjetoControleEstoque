f78e209327c3f46e84ff9f048841947eeef3f965
pedroldevjava <pedroldevjava@gmail.com>
2025-12-05 08:52:47 -0300

    ProjetoControleEstoque

---
+++ Produto.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProjetoControleEstoque.Entidade
+{
+     class Produto
+    {
+        //atributos
+        public string Nome;
+        public double Preco;
+        public int Quantidade;
+        // métodos
+
+       
+        public double ValorEmEstoque()
+        {
+            return Preco * Quantidade;
+
+        }
+        public void AdicionarProdutos(int quantity)
+        {
+            this.Quantidade += quantity;
+        }
+
+        public void RemoverProduto(int quantity)
+        {
+            this.Quantidade -= quantity;
+        }
+
+
+    }
+}
